ead05a58ac8b5c3076455ecaee69ea78c165638f
FevGrave <seanmattingly47@gmail.com>
2021-07-18 20:59:58 -0400

    Create SGO-M-RAB--Printer.bat
    This file needs 3 things you need to set the address in the bat file.
    Rename the "EDF Tools.exe" to "edftools.exe" so the M(rab) mass extract can happen
    and making a new folder for TOOLS and have both the SGOTT and EDF Tools in there

---
+++ SGO-M-RAB--Printer.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal
+set address=":\SteamLibrary\steamapps\common\EARTH DEFENSE FORCE 5\Mods\TOOLS"
+/ Set your path to the EDF 5 game file location
+:list
+echo 1. SGO
+echo 2. Stich SGO
+echo 3. M(RAB)
+echo 4. Exit Program 
+set /p x=Enter Unpacker file type 
+
+if %x% == 1 goto sgo-printer
+if %x% == 2 goto sgo-sticher
+if %x% == 3 goto m-rab-printer
+if %x% == 4 goto exit
+
+:sgo-printer
+for /r "%CD%" %%a in (*.sgo) do start /d %address% sgott.exe "%%~dpnxa"
+pause
+goto exit
+:sgo-sticher
+for /r "%CD%" %%a in (*.json) do start /d %address% sgott.exe "%%~dpnxa"
+pause
+goto list
+:m-rab-printer
+for /r "%CD%" %%a in (*.rab) do start /d %address% edftools.exe "%%~dpnxa"
+pause
+goto list
+
+pause
+goto list
+:exit
+exit
+:y
+cls
