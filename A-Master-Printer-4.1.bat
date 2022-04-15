c4c351f15bd252316260225b6c2373b8c97f9a60
FevGrave <seanmattingly47@gmail.com>
2022-04-14 20:17:47 -0400

    Update and rename SGO-M(RAB)-Printer-For-4.1.bat to A-Master-Printer-4.1.bat
    added support for the rest of the tools

---
+++ A-Master-Printer-4.1.bat
@@ -0,0 +1,109 @@
+@echo off
+setlocal
+set address=F:\SteamLibrary\steamapps\common\EARTH DEFENSE FORCE 5\Mods\TOOLS
+:: Set your path to the EDF 5 game file location
+echo V .311 A-Master-Printer
+
+:list
+echo 0. Search for gun name in json
+echo 1. SGO
+echo 2. Stitch SGO
+echo 3. Edit SGO
+echo 4. M(RAB) (sf)
+echo 5. M(RAB) Press-An-Hold any key if used
+echo 6. M(RAB) Stitch (sf)
+echo 7. AWB and AWE
+echo 8. ACB
+echo 9. RMPA and BVM (sf)
+echo 10. MAC
+echo 69. Exit Program 
+set /p x=Enter Unpacker file type 
+
+if %x% == 0 goto search
+if %x% == 1 goto sgo-printer
+if %x% == 2 goto sgo-stitcher
+if %x% == 3 goto sgo-editor
+if %x% == 4 goto m-rab-printerS
+if %x% == 5 goto m-rab-printer
+if %x% == 6 goto m-rab-stitcher
+if %x% == 7 goto AW~
+if %x% == 8 goto ACB
+if %x% == 9 goto RMPABVM
+if %x% == 10 goto MAC
+if %x% == 11 goto exit
+
+:loop
+echo.
+set /a loop=%loop%+1 
+if "%loop%"=="5" goto ANKELSNAPPER
+goto loop
+:ANKELSNAPPER
+set loop=0
+goto list
+:: Needs to be here or WE GOT A RUNNER
+
+:search
+set /p SEARCH=Name to the gun: 
+echo.
+echo File's that may have this: 
+FINDSTR /I /M %SEARCH% *.json
+goto loop
+
+:sgo-printer
+for /r "%CD%" %%a in (*.sgo) do "%address%\sgott.exe" "%%~dpnxa"
+echo Decrypting done . . . at: & time /t
+goto loop
+
+:sgo-stitcher
+for /r "%CD%" %%a in (*.json) do "%address%\sgott.exe" "%%~dpnxa"
+echo Encrypting done . . . at: & time /t
+goto loop
+
+:sgo-editor
+echo Opening SGO editor
+"%address%\edf-mod-tool.exe" "%%~dpnxa"
+goto loop
+
+:m-rab-printerS
+set /p cf=Enter file name: 
+"%address%\EDF Tools.exe" %cf% "%%~dpnxa"
+echo Decrypting done . . . at: & time /t
+goto loop
+
+:m-rab-printer
+for /r "%CD%" %%a in (*.rab, *.mrab) do "%address%\EDF Tools.exe" "%%~dpnxa"
+echo Decrypting done . . . at: & time /t
+goto loop
+
+:m-rab-stitcher
+set /p cf=Enter file name: 
+"%address%\EDF Tools.exe" /ARCHIVE %cf% "%%~dpnxa"
+echo Encrypting done . . . at: & time /t
+echo.
+set /p c=Do you have another file to be Archived (Y/N)?: 
+IF /I "%c%" NEQ "Y" GOTO loop
+goto m-rab-stitcher
+
+:AW~
+echo Opening Python Script : To go back, press enter 3 times
+python "%address%\awe_parser.py" "%%~dpnxa"
+goto loop
+
+:ACB
+set /p cf=Enter file name: 
+echo Opening EAT : To go back, Close and reopen this file
+"%address%\eternity_audio_tool.exe" %cf% "%%~dpnxa" goto loop
+
+:RMPABVM
+python "%address%\mission_tools.py"
+goto loop
+
+:MAC
+echo Opening Map editor : To go back, Close and reopen this file
+"%address%\MACfileTool_v1.0.0.exe" "%%~dpnxa"
+goto loop
+
+:exit
+exit
+:y
+cls
