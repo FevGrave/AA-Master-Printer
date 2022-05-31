86059cc352105d5e73cbc1f4752622556648c5eb
FevGrave <seanmattingly47@gmail.com>
2022-05-30 22:38:03 -0400

    deprecation was needed

--- CMD-Maker.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set count=770
-setlocal EnableDelayedExpansion
-
-for /L %%i in (1, 1, %count%) do (
-     set "formattedValue=000000%%i"
-     echo sgott.exe WEAPON!formattedValue:~-3!.SGO WEAPON!formattedValue:~-3!.json >> OUTPUT.txt
-)
-pause
