992ca6c42848c9f2f40b1bb17c11e9e23fcaf610
FevGrave <seanmattingly47@gmail.com>
2022-09-13 10:55:36 -0400

    Add files via upload
    1.0.343.14.2
    LOTS of changes 
    complimentary Python update script with batch file runner

---
+++ AA-Printer-Updater.py
@@ -0,0 +1,24 @@
+a_file = open("AA-Master-Printer.bat", "r")
+list_of_lines = a_file.readlines()
+
+# EDIT ME(AS AN EXAMPLE) anthing like this to a degree you need to change
+
+# 0 = Black       8 = Gray
+# 1 = Blue        9 = Light Blue
+# 2 = Green       A = Light Green
+# 3 = Aqua        B = Light Aqua
+# 4 = Red         C = Light Red
+# 5 = Purple      D = Light Purple0
+# 6 = Yellow      E = Light Yellow
+# 7 = White       F = Bright White
+list_of_lines[4] = "color " + "EDIT ME (0A)" + "\n"
+# First is background color, Second is text color as in HEX
+list_of_lines[5] = "set address=" + "EDIT ME(F:\SteamLibrary\steamapps\common\EARTH DEFENSE FORCE 5\Mods\TOOLS)" + "\n"
+# Set your path to the EDF 5 or 4.1 game file location
+# IF ERRORS SHOW UP you will need to duplicate all backslashes
+list_of_lines[6] = "MODE " + "EDIT ME(150)" + "," + "EDIT ME(60)" + "\n"
+# Sets window size First is Length then it's Height
+
+a_file = open("AA-Master-Printer.bat", "w")
+a_file.writelines(list_of_lines)
+a_file.close()
